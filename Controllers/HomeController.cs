540ed9e1f74ea0372c683b4bd016ed4992f25c21
bri <brijmcq@gmail.com>
2017-06-11 05:01:04 +0800

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_ng.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
